812f2666b202ee45082eaa500d28d9b1dc24cba8
Huyllee <moriarty1807@gmail.com>
2023-08-05 21:32:42 +0700

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./pages/public/Navbar";
+import Sidebar from "./pages/public/Sidebar";
+import Dashboard from "./pages/dashboard";
+// import Team from "./pages/team";
+// import Invoices from "./pages/invoices";
+// import Contacts from "./pages/contacts";
+// import Bar from "./pages/bar";
+// import Form from "./pages/form";
+// import Line from "./pages/line";
+// import Pie from "./pages/pie";
+// import FAQ from "./pages/faq";
+// import Geography from "./pages/geography";
+// import Calendar from "./pages/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,8 +23,22 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Navbar />
+            <Routes>
+              <Route path="/" Component={<Dashboard />} />
+              {/* <Route path="/team" Component={<Team />} />
+              <Route path="/invoices" Component={<Invoices />} />
+              <Route path="/contacts" Component={<Contacts />} />
+              <Route path="/bar" Component={<Bar />} />
+              <Route path="/form" Component={<Form />} />
+              <Route path="/line" Component={<Line />} />
+              <Route path="/pie" Component={<Pie />} />
+              <Route path="/faq" Component={<FAQ />} />
+              <Route path="/geography" Component={<Geography />} />
+              <Route path="/calendar" Component={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
--- src/pages/public/Sidebar/index.jsx
+++ src/pages/public/Sidebar/index.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
 
 const Sidebar = () => {
   return <div>Sidebar</div>;
