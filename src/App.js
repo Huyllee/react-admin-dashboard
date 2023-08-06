c2b5ca38ac62e7d287f4352bad1ca24292a3c6eb
Huyllee <moriarty1807@gmail.com>
2023-08-06 17:25:53 +0700

    create profile form page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Team from "./pages/team";
 import Invoices from "./pages/invoices";
 import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
-// import Form from "./pages/form";
+import Form from "./pages/form";
 // import Line from "./pages/line";
 // import Pie from "./pages/pie";
 // import FAQ from "./pages/faq";
@@ -31,9 +31,9 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/bar" element={<Bar />} />
+              {/* <Route path="/bar" element={<Bar />} /> */}
               <Route path="/form" element={<Form />} />
-              <Route path="/line" element={<Line />} />
+              {/* <Route path="/line" element={<Line />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
